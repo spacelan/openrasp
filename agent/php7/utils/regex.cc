64baf5a657fa15d87f708450751f30630ab4a583
lanyuhang <lanyuhang@baidu.com>
2018-12-25 11:36:25 +0800

    tmp

--- agent/php7/utils/regex.cc
+++ agent/php7/utils/regex.cc
@@ -17,88 +17,79 @@
 #include "openrasp.h"
 #include "regex.h"
 
-// extern "C"
-// {
-// #include "ext/pcre/php_pcre.h"
-// }
+extern "C"
+{
+#include "ext/pcre/php_pcre.h"
+}
 
 namespace openrasp
 {
 
+#if (PHP_MINOR_VERSION < 3)
+
+const static size_t OVECCOUNT = 30; /* should be a multiple of 3 */
+
 bool regex_match(const char *str, const char *regex, int options)
 {
-    return true;
+    pcre *re = nullptr;
+    int ovector[OVECCOUNT];
+    const char *error = nullptr;
+    int erroffset = 0;
+    int rc = 0;
+    re = pcre_compile(regex, options, &error, &erroffset, nullptr);
+    if (re == nullptr)
+    {
+        return false;
+    }
+    rc = pcre_exec(re, nullptr, str, strlen(str), 0, 0, ovector, OVECCOUNT);
+    pcre_free(re);
+
+    return rc >= 0;
 }
 
-// #if (PHP_MINOR_VERSION < 3)
+#else
 
-// const static size_t OVECCOUNT = 30; /* should be a multiple of 3 */
+bool regex_match(const char *str, const char *regex, int options)
+{
+    int errornumber;
+    PCRE2_SIZE erroroffset;
 
-// bool regex_match(const char *str, const char *regex, int options)
-// {
-//     pcre *re = nullptr;
-//     int ovector[OVECCOUNT];
-//     const char *error = nullptr;
-//     int erroffset = 0;
-//     int rc = 0;
-//     re = pcre_compile(regex, options, &error, &erroffset, nullptr);
-//     if (re == nullptr)
-//     {
-//         return false;
-//     }
-//     rc = pcre_exec(re, nullptr, str, strlen(str), 0, 0, ovector, OVECCOUNT);
-//     if (rc < 0)
-//     {
-//         pcre_free(re);
-//         return false;
-//     }
-//     pcre_free(re);
-//     return true;
-// }
+    pcre2_code *re = pcre2_compile(
+        (PCRE2_SPTR)regex,     /* the pattern */
+        PCRE2_ZERO_TERMINATED, /* indicates pattern is zero-terminated */
+        options,               /* default options */
+        &errornumber,          /* for error number */
+        &erroroffset,          /* for error offset */
+        nullptr);              /* use default compile context */
 
-// #else
+    /* Compilation failed: print the error message and exit. */
 
-// bool regex_match(const char *str, const char *regex, int options)
-// {
-//     int errornumber;
-//     PCRE2_SIZE erroroffset;
+    if (re == nullptr)
+    {
+        return false;
+    }
 
-//     pcre2_code *re = pcre2_compile(
-//         (PCRE2_SPTR)regex,     /* the pattern */
-//         PCRE2_ZERO_TERMINATED, /* indicates pattern is zero-terminated */
-//         options,               /* default options */
-//         &errornumber,          /* for error number */
-//         &erroroffset,          /* for error offset */
-//         nullptr);              /* use default compile context */
+    /* Using this function ensures that the block is exactly the right size for
+the number of capturing parentheses in the pattern. */
 
-//     /* Compilation failed: print the error message and exit. */
+    pcre2_match_data *match_data = pcre2_match_data_create_from_pattern(re, nullptr);
 
-//     if (re == nullptr)
-//     {
-//         return false;
-//     }
+    int rc = pcre2_match(
+        re,                    /* the compiled pattern */
+        (PCRE2_SPTR)str,       /* the subject string */
+        PCRE2_ZERO_TERMINATED, /* the length of the subject */
+        0,                     /* start at offset 0 in the subject */
+        0,                     /* default options */
+        match_data,            /* block for storing the result */
+        nullptr);              /* use default match context */
 
-//     /* Using this function ensures that the block is exactly the right size for
-// the number of capturing parentheses in the pattern. */
+    /* Matching failed: handle error cases */
+    pcre2_match_data_free(match_data); /* Release memory used for the match */
+    pcre2_code_free(re);               /* data and the compiled pattern. */
 
-//     pcre2_match_data *match_data = pcre2_match_data_create_from_pattern(re, nullptr);
+    return rc >= 0;
+}
 
-//     int rc = pcre2_match(
-//         re,                    /* the compiled pattern */
-//         (PCRE2_SPTR)str,       /* the subject string */
-//         PCRE2_ZERO_TERMINATED, /* the length of the subject */
-//         0,                     /* start at offset 0 in the subject */
-//         0,                     /* default options */
-//         match_data,            /* block for storing the result */
-//         nullptr);              /* use default match context */
-
-//     /* Matching failed: handle error cases */
-//     pcre2_match_data_free(match_data); /* Release memory used for the match */
-//     pcre2_code_free(re);               /* data and the compiled pattern. */
-
-//     return rc >= 0;
-// }
-
-// #endif // (PHP_MINOR_VERSION < 3)
+#endif // (PHP_MINOR_VERSION < 3)
 
 } // namespace openrasp
--- travis/php7/script.sh
+++ travis/php7/script.sh
@@ -3,5 +3,4 @@ set -ev
 
 pushd agent/$OPENRASP_LANG
 php run-tests.php -p `which php` -d extension=`pwd`/modules/openrasp.so --offline --show-diff --set-timeout 120
-php -d extension=`pwd`/modules/openrasp.so -m
 popd
